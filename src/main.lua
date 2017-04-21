422f9db47ee842ca4fb9f548d4762158d2142049
jgascacervantes <jgascacervantes@gmail.com>
2017-04-21 01:59:42 -0500

    add background image

--- src/main.lua
+++ src/main.lua
@@ -1,5 +1,6 @@
 function love.load()
-    
+    love.window.setMode(1280, 720, {resizable=true, vsync=false, minwidth=320, minheight=180})
+    bg = love.graphics.newImage("resources/kitchen 1280x720.png")
     im = love.graphics.newImage("resources/x2.png")
     memes = love.graphics.newParticleSystem( im, 10)
     memes:setParticleLifetime( 2, 5)
@@ -10,6 +11,7 @@ function love.load()
 end
 
 function love.draw()
+    love.graphics.draw( bg, 0, 0)
     love.graphics.draw( memes, 300, 300)
     love.graphics.draw( memes, 500, 250)
     love.graphics.draw( memes, 200, 150)
