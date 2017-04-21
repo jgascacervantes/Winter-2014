1e729117a27ac0e2788cc84b0e6ebc159573764e
jgascacervantes <jgascacervantes@gmail.com>
2017-04-21 02:47:03 -0500

    initial set up

--- src/main.lua
+++ src/main.lua
@@ -1,8 +1,8 @@
 function love.load()
-    love.window.setMode(1280, 720, {resizable=true, vsync=false, minwidth=320, minheight=180})
+    love.window.setMode(1280, 720, {resizable=false, vsync=false})
     bg = love.graphics.newImage("resources/kitchen 1280x720.png")
-    im = love.graphics.newImage("resources/x2.png")
-    memes = love.graphics.newParticleSystem( im, 10)
+    mc = love.graphics.newImage("resources/x4.png")
+    memes = love.graphics.newParticleSystem( mc, 10)
     memes:setParticleLifetime( 2, 5)
     memes:setSizeVariation(1)
     memes:setLinearAcceleration(-30, -30, 30, 30)
@@ -12,8 +12,8 @@ end
 
 function love.draw()
     love.graphics.draw( bg, 0, 0)
-    love.graphics.draw( memes, 300, 300)
-    love.graphics.draw( memes, 500, 250)
+    love.graphics.draw( memes, 700, 300)
+    love.graphics.draw( mc, 500, love.graphics.getHeight()/2 + 20)
     love.graphics.draw( memes, 200, 150)
 end
 
